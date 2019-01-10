b990b9f32b16d81f109744a2b4c7c200546982a7
Jerry <chengjinrui@babyfs.cn>
2019-01-10 22:01:36 +0800

    babyfs-miniprogram-cli init

---
+++ src/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mina: {
+    name: 'mina',
+    download: 'https://github.com/JerryCjr/babyfs-mina-template/archive/master.zip'
+  },
+
+  customComponent: {
+    name: 'custom-component',
+    download: 'https://github.com/wechat-miniprogram/miniprogram-custom-component/archive/master.zip',
+  },
+
+  miniprogram: {
+    name: 'miniprogram',
+    download: 'https://github.com/wechat-miniprogram/miniprogram-quickstart/archive/master.zip',
+  },
+
+  game: {
+    name: 'game',
+    download: 'https://github.com/wechat-miniprogram/miniprogram-game-quickstart/archive/master.zip',
+  },
+
+  plugin: {
+    name: 'plugin',
+    download: 'https://github.com/wechat-miniprogram/miniprogram-plugin-quickstart/archive/master.zip',
+  },
+};
